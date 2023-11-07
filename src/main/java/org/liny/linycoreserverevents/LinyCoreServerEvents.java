65ef49a851a533bbbc7eb02683f153d924105075
HikoMiko <butibutibgui@gmail.com>
2023-11-08 02:39:35 +0300

    Creating plugin

---
+++ src/main/java/org/liny/linycoreserverevents/LinyCoreServerEvents.java
@@ -0,0 +1,17 @@
+package org.liny.linycoreserverevents;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LinyCoreServerEvents extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
