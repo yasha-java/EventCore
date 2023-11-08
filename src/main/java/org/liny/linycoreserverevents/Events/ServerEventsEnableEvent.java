b7c2655af66cfc10fec56ae8b90fe42d61600f03
HikoMiko <butibutibgui@gmail.com>
2023-11-08 04:08:26 +0300

    Специальные эвенты что бы те плагины которые используют эту реализацию спавна эвентов могли отслеживать включение и выключение главного "Event-ядра"

--- src/main/java/org/liny/linycoreserverevents/Events/ServerEventsEnableEvent.java
+++ src/main/java/org/liny/linycoreserverevents/Events/ServerEventsEnableEvent.java
@@ -3,10 +3,23 @@ package org.liny.linycoreserverevents.Events;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+import org.liny.linycoreserverevents.LinyCoreServerEvents;
 
 public class ServerEventsEnableEvent extends Event {
 
     public final static HandlerList HANDLER_LIST = new HandlerList();
+
+    private final LinyCoreServerEvents main;
+
+    public ServerEventsEnableEvent (LinyCoreServerEvents main) {
+
+        this.main = main;
+
+    }
+
+    public LinyCoreServerEvents getMain () {
+        return this.main;
+    }
 
     public static HandlerList getHandlerList() {
         return HANDLER_LIST;
