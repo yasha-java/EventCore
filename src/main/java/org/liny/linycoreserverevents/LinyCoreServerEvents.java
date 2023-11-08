e4440a8bdb090f924655805ec5df7309afa51f83
HikoMiko <butibutibgui@gmail.com>
2023-11-08 03:29:28 +0300

    Главный класс объединяющий это все

--- src/main/java/org/liny/linycoreserverevents/LinyCoreServerEvents.java
+++ src/main/java/org/liny/linycoreserverevents/LinyCoreServerEvents.java
@@ -1,17 +1,71 @@
 package org.liny.linycoreserverevents;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.liny.linycoreserverevents.Commands.Events;
+import org.liny.linycoreserverevents.Events.ServerEventsDisableEvent;
+import org.liny.linycoreserverevents.Events.ServerEventsEnableEvent;
+import org.liny.linycoreserverevents.Managers.EventSpawnManager;
+import org.liny.linycoreserverevents.Utils.Math;
+
+import java.util.Objects;
 
 public final class LinyCoreServerEvents extends JavaPlugin {
 
+    private final Math math;
+    private final EventSpawnManager eventSpawnManager;
+    private final Events events;
+
+    public Boolean isStopped = true;
+
+    public LinyCoreServerEvents () {
+
+        this.math = new Math();
+        this.events = new Events(this);
+        this.eventSpawnManager = new EventSpawnManager(this);
+        this.eventSpawnManager.startMainScheduler();
+
+    }
+
+    public Math getMath () {
+
+        return this.math;
+
+    }
+
+    public EventSpawnManager getEventSpawnManager () {
+
+        return this.eventSpawnManager;
+
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        this.isStopped = false;
+
+        Objects.requireNonNull(getCommand("events")).setExecutor(this.events);
+
+        ServerEventsEnableEvent event = new ServerEventsEnableEvent();
+
+        Bukkit.getPluginManager().callEvent(event);
+
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "{LinyCoreEvents} LinyCoreEvents started!");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+        this.isStopped = true;
+
+        ServerEventsDisableEvent event = new ServerEventsDisableEvent();
+
+        Bukkit.getPluginManager().callEvent(event);
+
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "{LinyCoreEvents} LinyCoreEvents disabled!");
+
     }
+
 }
